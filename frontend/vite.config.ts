99026030fe3f3a72ee48a51036347f43128fd571
OBAMOS <patsah1717@gmail.com>
2026-01-16 18:40:13 -0700

    chore: deploy frontend to GitHub Pages (#7)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Engg1600-3D-MAP/",
   plugins: [react()],
   server: {
     port: 5173,
